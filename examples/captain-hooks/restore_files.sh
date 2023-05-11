cb7395c9c1552680210d08a7db974ce0e49eb72b
Rien <112709223+rieneverts@users.noreply.github.com>
2023-05-11 12:57:28 +0200

    Update restore_files.sh
    Documentation updated

--- examples/captain-hooks/restore_files.sh
+++ examples/captain-hooks/restore_files.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# recover the backup from the stream snapshot and save it in the migrate/data directory where edwh-migrate will pick it
-# up and restore it to the database. Instead of saving the file, you could also pipe the output to the psql application
-# make sure the filename (postgres.sql) matches the one used in the backup script.
+# recover the backup from the files snapshot and save it in ./
 
 restic $HOST -r $URI restore $SNAPSHOT --tag files --target ./ --exclude .git
