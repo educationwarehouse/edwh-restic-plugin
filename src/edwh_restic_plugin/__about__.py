2274c20faf92322ce9a0e4ae1e6130049b029a12
Robin van der Noord <robinvandernoord@gmail.com>
2026-08-13 19:17:47 +0200

    chore: switch to `vommit` for release management

--- src/edwh_restic_plugin/__about__.py
+++ src/edwh_restic_plugin/__about__.py
@@ -1,4 +1,6 @@
 # SPDX-FileCopyrightText: 2023-present Remco Boerma <remco.b@educationwarehouse.nl>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.16.1"
+from importlib.metadata import version
+
+__version__ = version("edwh-restic-plugin")
